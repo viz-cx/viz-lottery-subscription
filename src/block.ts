01b3a5c127918d993abb27ae9f27a8008d5cf2dc
Vladimir Babin <vovababin@gmail.com>
2021-11-04 00:23:15 +0500

    Add lottery logic

---
+++ src/block.ts
@@ -0,0 +1,15 @@
+import { readFileSync, WriteFileOptions, writeFileSync } from 'fs'
+
+export class CurrentBlock {
+    private fileName = `${__dirname}/../.block`
+    private options: WriteFileOptions = { encoding: 'utf-8' }
+
+    public save(block: number) {
+        writeFileSync(this.fileName, block.toString(), this.options)
+    }
+
+    public load(): number {
+        let block = readFileSync(this.fileName, this.options)
+        return parseInt(block.toString())
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,47 @@
+import { VIZ } from './viz'
+import { CurrentBlock } from './block'
+import * as dotenv from 'dotenv'
+dotenv.config({ path: `${__dirname}/../.env` })
+
 async function runApp() {
-    console.log("It works!")
+    let viz = new VIZ()
+    try {
+        let currentBlock = new CurrentBlock()
+        let blocksPerDay = 60 * 60 * 24 / 3
+        let nextBlock = currentBlock.load() + blocksPerDay
+        console.log('Checking next block', nextBlock)
+        let block = await viz.getBlockHeader(nextBlock)
+        if (block) {
+            let accountName = process.env.ACCOUNT
+            let account = await viz.getAccount(accountName)
+            let balance = parseFloat(account['balance'])
+            let activeSubscribers = (await viz.getPaidSubscriptionOptions(accountName))['active_subscribers']
+            let statuses = await Promise.all(activeSubscribers.map(subscriber => viz.getPaidSubscriptionStatus(subscriber, accountName)))
+            var participants: Object[] = []
+            for (let status of statuses) {
+                let subscriber = status['subscriber']
+                let level = status['level']
+                participants = participants.concat(Array(level).fill(subscriber))
+            }
+            const hashSumResult = hashSum(block['previous'] + block['witness'])
+            const winnerCode = hashSumResult % participants.length
+            const winner = participants[winnerCode]
+            console.log(winner)
+            // TODO: pay to winner
+            currentBlock.save(nextBlock)
+            // TODO: send post to FSP
+        }
+    } catch(err) {
+        console.log('Error:', err)
+        viz.changeNode()
+    }
 }
 
-runApp()
+function hashSum(s: string): number {
+    return s.split('').reduce(function (a, b) {
+        a = ((a << 5) - a) + b.charCodeAt(0)
+        return Math.abs(a & a)
+    }, 0)
+}
+
+setInterval(runApp, 1000 * 10)
